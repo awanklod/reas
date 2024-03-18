378fafe60db350e32ca2268547e459de7866018d
awanklod <137737752+awanklod@users.noreply.github.com>
2024-03-18 10:52:28 +0700

    Update insshws.sh

--- sshws/insshws.sh
+++ sshws/insshws.sh
@@ -32,32 +32,32 @@ systemctl enable ws-dropbear.service
 systemctl start ws-dropbear.service
 systemctl restart ws-dropbear.service
 
-#wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/awanklod/reas/main/sshws/ws-ovpn.py
-#chmod +x /usr/local/bin/ws-ovpn
+wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/awanklod/reas/main/sshws/ws-ovpn.py
+chmod +x /usr/local/bin/ws-ovpn
 
-# Installing Service
-#cat > /etc/systemd/system/ws-ovpn.service << END
-#[Unit]
-#Description=Python Proxy Mod By SFVPN
-#Documentation=https://t.me/abecasdee13
-#After=network.target nss-lookup.target
+ Installing Service
+cat > /etc/systemd/system/ws-ovpn.service << END
+[Unit]
+Description=Python Proxy Mod By SFVPN
+Documentation=https://t.me/abecasdee13
+After=network.target nss-lookup.target
 
-#[Service]
-#Type=simple
-#User=root
-#CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-#AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
-#NoNewPrivileges=true
-#ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
-#Restart=on-failure
+[Service]
+Type=simple
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=/usr/bin/python -O /usr/local/bin/ws-ovpn 2086
+Restart=on-failure
 
-#[Install]
-#WantedBy=multi-user.target
-#END
+[Install]
+WantedBy=multi-user.target
+END
 
-#systemctl daemon-reload
-#systemctl enable ws-ovpn
-#systemctl restart ws-ovpn
+systemctl daemon-reload
+systemctl enable ws-ovpn
+systemctl restart ws-ovpn
 
 wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/awanklod/reas/main/sshws/ws-stunnel
 chmod +x /usr/local/bin/ws-stunnel
