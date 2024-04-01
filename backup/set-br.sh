26b4cae07151b0b242259a74ebe30bdc60b8cf54
awanklod <137737752+awanklod@users.noreply.github.com>
2024-04-01 16:43:42 +0700

    Update set-br.sh

--- backup/set-br.sh
+++ backup/set-br.sh
@@ -2,15 +2,15 @@
 
 apt install rclone
 printf "q\n" | rclone config
-wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/RMBL-VPN/v/main/install/rclone.conf"
+wget -O /root/.config/rclone/rclone.conf "https://raw.githubusercontent.com/awanklod/reas/main/backup/rclone.conf"
 git clone  https://github.com/casper9/wondershaper.git
 cd wondershaper
 make install
 cd
 rm -rf wondershaper
 cd /usr/bin
-wget -O backup "https://raw.githubusercontent.com/RMBL-VPN/v/main/menu/backup.sh"
-wget -O restore "https://raw.githubusercontent.com/RMBL-VPN/v/main/menu/restore.sh"
+wget -O backup "https://raw.githubusercontent.com/awanklod/reas/main/menu/backup.sh"
+wget -O restore "https://raw.githubusercontent.com/awanklod/reas/main/menu/restore.sh"
 wget -O cleaner "https://raw.githubusercontent.com/awanklod/reas/main/backup/cleaner.sh"
 wget -O xp "https://raw.githubusercontent.com/RMBL-VPN/v/main/install/xp.sh"
 chmod +x /usr/bin/backup
