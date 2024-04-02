703beafcc4a3d8536f3f5059b72b2fc6ee3aed94
awanklod <137737752+awanklod@users.noreply.github.com>
2024-04-02 10:47:08 +0700

    Update backup.sh

--- menu/backup.sh
+++ menu/backup.sh
@@ -55,7 +55,7 @@ cp -r /etc/xray/sshx /root/backup/sshx &> /dev/null
 cp -r /etc/vmess /root/backup/vmess &> /dev/null
 cp -r /etc/vless /root/backup/vless &> /dev/null
 cp -r /etc/trojan /root/backup/trojan &> /dev/null
-cp -r etc/trojan-go/trgo /root/backup/trgo &> /dev/null
+cp -r etc/trgo /root/backup/trgo &> /dev/null
 cp -r /etc/noobz /root/backup/noobz &> /dev/null
 cp -r /etc/issue.net /root/backup/issue &> /dev/null
 cd /root
