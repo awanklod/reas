ca08dc79620c9960de71382d7de134cf910f2530
awanklod <137737752+awanklod@users.noreply.github.com>
2024-04-10 02:44:01 +0700

    Update xp.sh

--- ssh/xp.sh
+++ ssh/xp.sh
@@ -21,6 +21,19 @@ rm /etc/xray/sshx/${user}IP >/dev/null 2>&1
 rm /etc/xray/sshx/${user}login >/dev/null 2>&1
 fi
 done
+data=( `cat /etc/xray/noob | grep '^###' | cut -d ' ' -f 2 | sort | uniq`);
+now=`date +"%Y-%m-%d"`
+for user in "${data[@]}"
+do
+exp=$(grep -w "^### $user" "/etc/xray/noob" | cut -d ' ' -f 3 | sort | uniq)
+d1=$(date -d "$exp" +%s)
+d2=$(date -d "$now" +%s)
+exp2=$(( (d1 - d2) / 86400 ))
+if [[ "$exp2" -le "0" ]]; then
+sed -i "/^### $user $exp/,/^},{/d" /etc/xray/noob
+noobzvpns --remove-user "$user"
+fi
+done
 data=( `cat /etc/xray/config.json | grep '^#vmg' | cut -d ' ' -f 2 | sort | uniq`);
 now=`date +"%Y-%m-%d"`
 for user in "${data[@]}"
