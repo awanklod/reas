145d1fea3e2b2cf1fa9ceaf5e81ffdbd0f3a6d9b
awanklod <137737752+awanklod@users.noreply.github.com>
2024-04-25 02:26:49 +0700

    Update m-allxray.sh

--- menu/m-allxray.sh
+++ menu/m-allxray.sh
@@ -10,10 +10,10 @@ COLBG1="$(cat /etc/rmbl/theme/$colornow | grep -w "BG" | cut -d: -f2|sed 's/ //g
 WH='\033[1;37m'
 ###########- END COLOR CODE -##########
 
-ipsaya=$(wget -qO- ipv4.icanhazip.com)
+ipsaya=$(curl -sS ipv4.icanhazip.com)
 data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
 date_list=$(date +"%Y-%m-%d" -d "$data_server")
-data_ip="https://raw.githubusercontent.com/awanklod/reas/main/izin"
+data_ip="https://raw.githubusercontent.com/awanklod/izin_reaz/main/izin"
 checking_sc() {
     useexp=$(curl -sS $data_ip | grep $ipsaya | awk '{print $3}')
     if [[ $date_list < $useexp ]]; then
