aa2c6d14b30c1c73d6549a060f71e21964c28414
awanklod <137737752+awanklod@users.noreply.github.com>
2024-04-29 04:02:26 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -463,7 +463,7 @@ clear
 }
 
 res6() {
-wget https://raw.githubusercontent.com/awanklod/reas/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
+#wget https://raw.githubusercontent.com/awanklod/reas/main/ohp/ohp.sh && chmod +x ohp.sh && ./ohp.sh
 clear
 }
 
