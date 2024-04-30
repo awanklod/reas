8dce6d647f4dedd092e05f1962a289a0f81e8381
awanklod <137737752+awanklod@users.noreply.github.com>
2024-05-01 05:06:09 +0700

    Update m-vmess.sh

--- menu/m-vmess.sh
+++ menu/m-vmess.sh
@@ -70,7 +70,7 @@ fi
 done
 uuid=$(cat /proc/sys/kernel/random/uuid)
 #read -p " CREAT PW (OTOMATIC RANDOM PW) :" uuid
-    [[ -z "$uuid" ]] && uuid=`cat /proc/sys/kernel/random/uuid`
+#    [[ -z "$uuid" ]] && uuid=`cat /proc/sys/kernel/random/uuid`
 until [[ $masaaktif =~ ^[0-9]+$ ]]; do
 read -p "Expired (hari): " masaaktif
 done
